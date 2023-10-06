0d462775217891c24a858c5fc2b519b7ad095086
Ignacio <ignaciosabiajes@gmail.com>
2023-10-06 10:51:11 -0300

    some more try and error

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+  <button>{{value}}</button>
+  `,
+  styleUrls: ['./square.component.scss']
+})
+export class SquareComponent {
+
+  @Input() value: 'X' | 'O' | any;
+
+}
